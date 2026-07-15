854c0e7877cbca5b3afb15233bcf8f19486f2201
sushma1002ga <sushma1002ga@gmail.com>
2026-07-15 23:52:38 +0530

    Update DNS validation records in main.tf

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -21,7 +21,7 @@ resource "aws_acm_certificate" "primary" {
 # DNS Validation Records
 resource "aws_route53_record" "primary_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.primary.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.primary.domain_validation_options :dvo.resource_record_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
